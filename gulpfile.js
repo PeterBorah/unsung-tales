afe081f8451790eb39e66c4559672b36bc88049c
Peter Borah <peterborah@gmail.com>
2014-01-31 18:00:32 +0000

    Set up gulp for concatination and template rendering

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var handlebars = require('gulp-ember-handlebars');
+
+gulp.task('templates', function(){
+  gulp.src(['js/templates/*.hbs'])
+    .pipe(handlebars({
+      outputType: 'browser'
+     }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('scripts', function(){
+  gulp.src(['js/**/*.js'])
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('default', function(){
+  gulp.start('templates', 'scripts');
+});
